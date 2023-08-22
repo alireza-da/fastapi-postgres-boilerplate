b3ab4aae0355e4b419942b63894d4a2d7685039b
Alireza Davoodi <alirezadavoodi1378@gmail.com>
2023-08-22 17:58:04 +0330

    fix: model foreign keys & add functions as service

--- app/app/api/api_v1/endpoints/books.py
+++ app/app/api/api_v1/endpoints/books.py
@@ -19,6 +19,7 @@ from fastapi_jwt_auth.exceptions import AuthJWTException
 
 from app import crud, models, schemas, utils
 from app.api import deps
+from app.api.api_v1 import service
 from app.core import security, library_management
 from app.core.config import settings
 from app.core.security import get_password_hash
@@ -73,77 +74,14 @@ async def get_all_taken_books(
 async def create_taken_book(*, db: AsyncSession = Depends(deps.get_db_async), book_in: schemas.TakenBook):
     user = await crud.user.get_by_id(db, book_in.user)
     tbs = await crud.taken_book.get_by_user(db, user.id)
-    # check if user has any overdue book
-    for tb in tbs:
-        if tb.status == schemas.book.TakenBookStatus().OVERDUE:
-            raise exc.InternalServiceError(
-                status_code=400,
-                detail="The user can't borrow book [User has undelivered overdue book(s)]",
-                msg_code=utils.MessageCodes.bad_request,
-            )
-    db_book = await crud.book.get_by_id(db, book_in.book)
-    book = schemas.Book.from_db(db_obj=db_book)
-    # check if user can borrow from selected category
-    if await library_management.can_borrow_from_cat(db, book, user):
-        days = await library_management.find_valid_rent_days(db, db_book)
-        category = await crud.category.get_by_id(db, book.category)
-        if days == 0:
-            raise exc.InternalServiceError(
-                status_code=400,
-                detail="The user can't borrow book [Target Books have been rented: Out of Capacity]",
-                msg_code=utils.MessageCodes.bad_request,
-            )
+    return APIResponse(await service.rent_book(db, user, tbs, book_in))
 
-        elif user.balance < 3 * category.rent_price:
-            raise exc.InternalServiceError(
-                status_code=400,
-                detail="The user can't borrow book [Insufficient Account Balance]",
-                msg_code=utils.MessageCodes.bad_request,
-            )
-        book_in.valid_borrowed_days = days
-        book.amount -= 1
-        await crud.book.update(db, db_obj=db_book, obj_in=book)
-        created_tb = await crud.taken_book.create(db, obj_in=book_in)
-        return APIResponse(created_tb)
-    raise exc.InternalServiceError(
-            status_code=400,
-            detail="The user can't borrow book [Category Limit]",
-            msg_code=utils.MessageCodes.bad_request,
-     )
 
 @router.post('/taken-books/return-book')
 async def return_taken_book(*, db:AsyncSession=Depends(deps.get_db_async), taken_book_id: int):
     msg = "Book delivered successfully."
     db_taken_book = await crud.taken_book.get_by_id(db, taken_book_id)
-    if db_taken_book.status == schemas.book.TakenBookStatus().RECEIVED \
-            or db_taken_book.status == schemas.book.TakenBookStatus().OVERDUE_DELIVERED:
-        return {"msg": "Book already been delivered"}
-    user = await crud.user.get_by_id(db, db_taken_book.user)
-    current_user_data = jsonable_encoder(user)
-    user_in = schemas.UserUpdate(**current_user_data)
-    taken_book = schemas.TakenBook.from_db(db_obj=db_taken_book)
-    db_book = await crud.book.get_by_id(db, taken_book.book)
-    book = schemas.Book.from_db(db_obj=db_book)
-    category = await crud.category.get_by_id(db, book.category)
-    taken_book.returning_date = datetime.datetime.now()
-    naive_td = taken_book.taken_date.replace(tzinfo=tz.tzutc())
-    naive_rd = taken_book.returning_date.replace(tzinfo=tz.tzutc())
-    diff = naive_rd - naive_td
-    thirty_days = timedelta(days=taken_book.valid_borrowed_days)
-    if diff <= thirty_days:
-        taken_book.status = schemas.book.TakenBookStatus().RECEIVED
-    elif diff > thirty_days:
-        taken_book.status = schemas.book.TakenBookStatus().OVERDUE_DELIVERED
-        user_in.balance -= category.overdue_penalty
-        taken_book.bill += category.overdue_penalty
-        await crud.user.update(db, db_obj=user, obj_in=user_in)
-        msg += f"\nOverdue Delivery({diff.days} days), User received a penalty of {category.overdue_penalty}"
-    book.amount += 1
-    await crud.book.update(db, db_obj=db_book, obj_in=book)
-    taken_book.taken_date = taken_book.taken_date.strftime('YYYY-MM-DDTHH:MM:SS')
-    taken_book.returning_date = taken_book.returning_date.strftime('YYYY-MM-DDTHH:MM:SS')
-    await crud.taken_book.update(db, db_obj=db_taken_book, obj_in=taken_book)
-    return {"msg": msg, "details": taken_book}
+    return await service.return_book(db, msg, db_taken_book)
 
 @router.put('/taken-books/update')
 async def update_taken_book(*, db:AsyncSession=Depends(deps.get_db_async),
@@ -235,35 +173,7 @@ async def get_available_books(*, db: AsyncSession = Depends(deps.get_db_async)):
 async def sell_book(*, db: AsyncSession = Depends(deps.get_db_async), book_id: int, user_id: int):
     user = await crud.user.get_by_id(db, user_id)
     book = await crud.book.get_by_id(db, book_id)
-    if not book.sell_price:
-        book.sell_price = 1
-    if user.balance > book.sell_price:
-        if book.amount > 0:
-            current_user_data = jsonable_encoder(user)
-            user_in = schemas.UserUpdate(**current_user_data)
-            user_in.balance -= book.sell_price
-            _book = schemas.book.Book.from_db(db_obj=book)
-            _book.amount -= 1
-            now = datetime.datetime.now()
-            taken_book = schemas.book.TakenBook(user=user_id, book=book_id,
-                                                taken_date=now, returning_date=now,
-                                                valid_borrowed_days=-1, bill=book.sell_price,
-                                                status= schemas.book.TakenBookStatus().SOLD)
-            await crud.taken_book.create(db, obj_in=taken_book)
-            await crud.user.update(db, db_obj=user, obj_in=user_in)
-            return {"msg": f"Book[{book.name}({book.id})] has been sold to {user.email} | Price: {book.sell_price}"}
-        else:
-            raise exc.InternalServiceError(
-                status_code=400,
-                detail="The user can't buy book [Book Amount Limit]",
-                msg_code=utils.MessageCodes.bad_request,
-            )
-    else:
-        raise exc.InternalServiceError(
-            status_code=400,
-            detail="The user can't buy book [Insufficient Account Balance]",
-            msg_code=utils.MessageCodes.bad_request,
-        )
+    return await service.sell_book(db, user, book)
 
 @router.get('/statistics')
 async def get_statistics(*, db: AsyncSession = Depends(deps.get_db_async)):
@@ -290,15 +200,10 @@ async def get_statistics(*, db: AsyncSession = Depends(deps.get_db_async)):
             rents_overall_gain += tb.bill
         else:
             in_rents.append(tb)
-
     return {"msg": "Statistics Calculated",
             "sell": f"Sold {len(sells)} books | Gain: {sells_overall_gain}$",
             "rent": f"Loaned {len(rents)} books | Gain: {rents_overall_gain}$ & {len(in_rents)} books have been rented but not delivered yet ",
-            # "sell_data": sells,
-            # "rent_data": rents,
-            # "in_rent_data": in_rents,
-            "category_classified": cats_profit
-            }
+            "category_classified": cats_profit}
 
 
 @router.get('/find-users-violations')
